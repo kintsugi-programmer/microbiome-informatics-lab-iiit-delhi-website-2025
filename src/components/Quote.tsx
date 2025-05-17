b23a6545a50262b80a759667dab7e09881f10682
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-05-18 01:59:05 +0530

    Add Quote1 Quote2

---
+++ src/components/Quote.tsx
@@ -0,0 +1,49 @@
+
+export const Quote1 = () => {
+    return (
+      <section className="bg-white dark:bg-teal-900">
+      <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+          <figure className="max-w-screen-md mx-auto">
+              <svg className="h-12 mx-auto mb-3 text-teal-400 dark:text-teal-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
+              </svg> 
+              <blockquote>
+                  <p className="sm:text-2xl text-xl font-medium text-teal-900 dark:text-white">
+                  &quot;Your success is our purpose. At MI Lab, we don&apos;t just deliver services—we build partnerships that drive growth, ensure compliance, and create lasting value.&quot;</p>
+              </blockquote>
+              <figcaption className="flex items-center justify-center mt-6 space-x-3">
+                  <img className="w-12 h-12 rounded-full " src="/img/tarini.png" alt="profile picture"></img>
+                  <div className="flex items-center divide-x-2 divide-teal-500 dark:divide-teal-700">
+                      <div className="pr-3 font-medium text-teal-900 dark:text-white">Dr.Tarini Shankar Ghosh</div>
+                      <div className="pl-3 text-sm font-light text-teal-500 dark:text-teal-400">Proprietor</div>
+                  </div>
+              </figcaption>
+          </figure>
+      </div>
+    </section>
+    )
+  }
+export const Quote2 = () => {
+    return (
+      <section className="bg-teal-700 rounded-xl">
+      <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+          <figure className="max-w-screen-md mx-auto">
+              <svg className="h-12 mx-auto mb-3 text-white" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
+              </svg> 
+              <blockquote>
+                  <p className="sm:text-2xl text-xl font-medium text-white">
+                  &quot;Your success is our purpose. At MI Lab, we don&apos;t just deliver services—we build partnerships that drive growth, ensure compliance, and create lasting value.&quot;</p>
+              </blockquote>
+              <figcaption className="flex items-center justify-center mt-6 space-x-3">
+                  <img className="w-12 h-12 rounded-full " src="/img/tarini.png" alt="profile picture"></img>
+                  <div className="flex items-center divide-x-2 divide-gray-200">
+                      <div className="pr-3 font-medium text-white">Dr.Tarini Shankar Ghosh</div>
+                      <div className="pl-3 text-sm font-light text-gray-200">Proprietor</div>
+                  </div>
+              </figcaption>
+          </figure>
+      </div>
+    </section>
+    )
+  }
