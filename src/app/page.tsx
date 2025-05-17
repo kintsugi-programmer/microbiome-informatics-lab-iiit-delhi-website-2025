4a81c62b8a53e9536c977c70bb98f816a322970c
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-05-18 02:00:01 +0530

    Modify All for Quotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import { Quote1,Quote2 } from '@/components/Quote'
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col p-5 space-y-5">
     <Hero/>
-    </>
+    <Quote1/>
+    <Quote2/>
+    </div>
   );
 }
--- src/app/temp1/page.tsx
+++ src/app/temp1/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-
+import { Quote1,Quote2 } from '@/components/Quote'
 
 
 // py-40 parent 
@@ -82,52 +82,3 @@ const page = () => {
 }
 
 export default page
-
-const Quote1 = () => {
-    return (
-      <section className="bg-white dark:bg-gray-900">
-      <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
-          <figure className="max-w-screen-md mx-auto">
-              <svg className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
-              </svg> 
-              <blockquote>
-                  <p className="sm:text-2xl text-xl font-medium text-gray-900 dark:text-white">
-                  &quot;Your success is our purpose. At MI Lab, we don&apos;t just deliver services—we build partnerships that drive growth, ensure compliance, and create lasting value.&quot;</p>
-              </blockquote>
-              <figcaption className="flex items-center justify-center mt-6 space-x-3">
-                  <img className="w-12 h-12 rounded-full " src="/logo.png" alt="profile picture"></img>
-                  <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                      <div className="pr-3 font-medium text-gray-900 dark:text-white">Dr.Tarini Shankar Ghosh</div>
-                      <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">Proprietor</div>
-                  </div>
-              </figcaption>
-          </figure>
-      </div>
-    </section>
-    )
-  }
-const Quote2 = () => {
-    return (
-      <section className="bg-blue-700 rounded-xl">
-      <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
-          <figure className="max-w-screen-md mx-auto">
-              <svg className="h-12 mx-auto mb-3 text-white" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
-              </svg> 
-              <blockquote>
-                  <p className="sm:text-2xl text-xl font-medium text-white">
-                  &quot;Your success is our purpose. At MI Lab, we don&apos;t just deliver services—we build partnerships that drive growth, ensure compliance, and create lasting value.&quot;</p>
-              </blockquote>
-              <figcaption className="flex items-center justify-center mt-6 space-x-3">
-                  <img className="w-12 h-12 rounded-full " src="/logo.png" alt="profile picture"></img>
-                  <div className="flex items-center divide-x-2 divide-gray-200">
-                      <div className="pr-3 font-medium text-white"> Dr.Tarini Shankar Ghosh</div>
-                      <div className="pl-3 text-sm font-light text-gray-200">Proprietor</div>
-                  </div>
-              </figcaption>
-          </figure>
-      </div>
-    </section>
-    )
-  }
