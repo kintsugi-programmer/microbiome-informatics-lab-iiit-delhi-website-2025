bab1875e805385f5932d474d933e27175ebb40fa
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-05-18 22:02:18 +0530

    Modify config Add Export Static Out

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {output: 'export', // Enable static export
+  // basePath: '/dss',  // Add this line
+//   experimental: {
+//     appDir: true,  // Enable if you're using App router
+//   },
+  trailingSlash: true, // Optional: ensures all routes have a trailing slash in the URL
+    images: {
+      unoptimized: true,
+      },};
 
 export default nextConfig;
